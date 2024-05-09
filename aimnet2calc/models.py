619aebcf2a35dc102d395b6c5292027b7b53983d
zubatyuk <zubatyuk@gmail.com>
2024-05-08 21:38:43 -0400

    add qr model

--- aimnet2calc/models.py
+++ aimnet2calc/models.py
@@ -3,9 +3,10 @@ import requests
 
 # model registry aliases
 model_registry_aliases = {}
-model_registry_aliases['aimnet2'] = 'aimnet2_wb97m_0'
+model_registry_aliases['aimnet2'] = 'aimnet2/aimnet2_wb97m_0'
 model_registry_aliases['aimnet2_wb97m'] = model_registry_aliases['aimnet2']
-model_registry_aliases['aimnet2_b973c'] = 'aimnet2_b973c_0'
+model_registry_aliases['aimnet2_b973c'] = 'aimnet2/aimnet2_b973c_0'
+model_registry_aliases['aimnet2-qr'] = 'aimnet2-qr/aimnet2-qr_b97md4_qzvp_2'
 
 
 def get_model_path(s: str):
@@ -19,12 +20,13 @@ def get_model_path(s: str):
     # add jpt extension
     if not s.endswith('.jpt'):
         s = s + '.jpt'
-    os.makedirs(os.path.join(os.path.dirname(__file__), 'assets'), exist_ok=True)
+    sdir = os.path.dirname(s)
+    os.makedirs(os.path.join(os.path.dirname(__file__), 'assets', sdir), exist_ok=True)
     s_local = os.path.join(os.path.dirname(__file__), 'assets', s)
     if os.path.isfile(s_local):
         print('Found model file:', s_local)
     else:
-        url = f'https://github.com/zubatyuk/aimnet-model-zoo/raw/main/aimnet2/{s}'
+        url = f'https://github.com/zubatyuk/aimnet-model-zoo/raw/main/{s}'
         print('Downloading model file from', url)
         r = requests.get(url)
         r.raise_for_status()
