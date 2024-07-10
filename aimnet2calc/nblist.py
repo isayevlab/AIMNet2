9d006e17d7060570cafb03c5094a45651bce3982
zubatyuk <zubatyuk@gmail.com>
2024-07-10 11:19:34 -0400

    fix nblist for isolated atoms case

--- aimnet2calc/nblist.py
+++ aimnet2calc/nblist.py
@@ -32,8 +32,10 @@ def nblist_torch_cluster(coord: Tensor, cutoff: float, mol_idx: Optional[Tensor]
     assert coord.shape[0] < 2147483646, 'Too many atoms, max supported is 2147483646'
     max_num_neighbors = max_nb
     while True:
-        sparse_nb = radius_graph(coord, batch=mol_idx, r=cutoff, max_num_neighbors=max_nb).to(torch.int32)
-        max_num_neighbors = torch.unique(sparse_nb[0], return_counts=True)[1].max().item()
+        nnb = torch.unique(sparse_nb[0], return_counts=True)[1]
+        if nnb.numel() == 0:
+            break
+        max_num_neighbors = nnb.max().item()
         if max_num_neighbors < max_nb:
             break
         max_nb *= 2
